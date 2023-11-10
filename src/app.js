aabb8922222a65ce98e02d845886b637e5fdc5e8
laurammoraes2@gmail.com <laura@poontoagencia.com>
2023-11-10 06:41:24 -0300

    Montando a estrutura do app

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const config = require('./config');
+// const loaders = require('./loaders')
+const express = require('express')
+
+
+
+async function startServer(){
+
+
+
+const app = express()
+
+
+// await loaders.init({expressApp: app});
+
+
+app.listen(config.port, err => {
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log(`Example app listening on port ${config.port}`)
+})
+
+};
+
+startServer();
