495b658ec79e989e2049d92e3a2517818894b996
laurammoraes2@gmail.com <laura@poontoagencia.com>
2023-11-11 05:41:01 -0300

    server funcionando e estrutura de pastas montada

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 const envFound = dotenv.config();
+
 if (envFound.error) {
   // This error should crash whole process
 
